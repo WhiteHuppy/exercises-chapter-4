5cdb02683a6df01ee01002da0549ba7a34a9841f
Nikhil Ramchandran <nikhil.ramchandran18@imperial.ac.uk>
2022-02-12 16:21:02 +0000

    Fixed flake8 errors.

--- life/life.py
+++ life/life.py
@@ -1,13 +1,13 @@
-import numpy as np
+import numpy as np  # noqa D100
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]])
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -48,30 +48,40 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    """Play the Game of Life."""
+
+    def __init__(self, size):
+        """Construct board of dimensions size x size."""
+        self.board = np.zeros((size, size))
+
     def play(self):
+        """Play the Game of Life indefinitely."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        """Determine the states of the squares in the new step."""
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (neighbourcount[i, j] == 3
+                                         or (neighbourcount[i, j] == 2
+                                             and self.board[i, j])) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """Assign a board square a value."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Show board using matplotlib."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
